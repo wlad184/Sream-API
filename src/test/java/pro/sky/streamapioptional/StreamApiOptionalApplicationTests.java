6e18e6dbcd49bd3fbbc0b55111a41aa7c508bfa9
wlad184 <evel184@yandex.ru>
2023-04-20 12:14:14 +0700

    Stream API

---
+++ src/test/java/pro/sky/streamapioptional/StreamApiOptionalApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.streamapioptional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamApiOptionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
